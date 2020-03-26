a6e5416c9ce223626034e19e8740340bb09bd925
Emeric-gui <61002169+Emeric-gui@users.noreply.github.com>
2020-03-26 12:48:10 +0100

    Add files via upload
    TP 7 PHP

---
+++ TP07/testform2.php
@@ -0,0 +1,15 @@
+<?php
+
+include ("form2.php");
+
+$form = new form2('POST','');
+$form->ajouterzonetexte('Votre nom');
+$form->ajouterzonetexte('Votre code');
+$form->ajouterRadio('Homme');
+$form->ajouterRadio('Femme');
+$form->ajouterCheckbox('Tennis');
+$form->ajouterCheckbox('Equitation');
+$form->ajouterbouton();
+$form->getform();
+
+?>
